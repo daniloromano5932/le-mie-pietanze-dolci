a5a6d7053af04e3d84d2e8b2d345b1cccc97daa5
daniloromano5932 <110908582+daniloromano5932@users.noreply.github.com>
2023-06-09 20:13:25 +0200

    added css to home page

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -5,9 +5,9 @@ import Card from "../components/Card";
 
 function Categories() {
   return (
-      <div className="test ">
+      <div className="test">
     <Container className="colored-section">
-           <Row lg={4} xl={4} md={4} className="categories-cards align-items-center justify-content-center">
+           <Row lg={4} xl={5} md={4} xxl={5} className="categories-cards align-items-center justify-content-center">
              {HomePageCategories.map((category) => (
               <Col>
                <Card
