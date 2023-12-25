266504183d82f0a259a7e725c093a44fc73f15ad
daniloromano5932 <110908582+daniloromano5932@users.noreply.github.com>
2023-12-25 12:40:39 +0100

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Home from './pages/Home';
@@ -13,6 +14,7 @@ function App() {
   
   return (
     <BrowserRouter>
+      <Analytics />
       <Navbar />
       <main className='main'>
         <Routes>
